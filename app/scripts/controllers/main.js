01d4bceb3a0eb093f8bf8466c99bb9767e0008ec
John VanDeWeghe <vandeweghe@lunixlabs.com>
2015-10-17 03:11:52 -0500

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lunixLabsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lunixLabsApp
+ */
+angular.module('lunixLabsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
